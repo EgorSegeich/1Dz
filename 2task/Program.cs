49ff80ce8ee7e065582230b704e6535ae832ead1
Egor <aldes@list.ru>
2022-12-03 19:24:56 +0300

    выполнено 2 задание

---
+++ 2task/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("введите первое число: ");
+int N1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("введите второе число: ");
+int N2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("введите третье число: ");
+int N3 = Convert.ToInt32(Console.ReadLine());
+
+int max = N1;
+
+if(max<N2)
+{
+max = N2;   
+}
+if(max<N3)
+{
+max = N3;
+}
+Console.Write("max=");
+Console.WriteLine(max);
