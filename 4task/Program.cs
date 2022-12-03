7851076958a25d3acac9b149ab94f27b3d352e54
Egor <aldes@list.ru>
2022-12-03 20:44:38 +0300

    выполнено 4 задание

---
+++ 4task/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+double i = 1;
+
+while (i <= N)
+{
+    if((i%2)==0)
+    {
+    Console.WriteLine(i);
+    }
+i++;
+}
