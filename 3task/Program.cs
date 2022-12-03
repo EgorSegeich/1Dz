42c21f914917a2a52a69151fa8424de123f887bc
Egor <aldes@list.ru>
2022-12-03 19:58:43 +0300

    выполнено 3 задание

---
+++ 3task/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("введите число: ");
+double N = Convert.ToInt32(Console.ReadLine());
+double C = N % 2;
+
+if(C==0)
+{
+Console.WriteLine("да");
+}
+else
+{
+Console.WriteLine("нет");
+}
